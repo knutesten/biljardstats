6b2dc7898a5ebaae7831b05c6e5d3c18832a3bb1
Knut Esten Melandsø Nekså <knut.neksa@gmail.com>
2017-06-03 22:59:39 +0200

    Fixed formatting

--- src/main/kotlin/com/billiardsstats/web/websocket/SessionManager.kt
+++ src/main/kotlin/com/billiardsstats/web/websocket/SessionManager.kt
@@ -1,13 +1,7 @@
 package com.billiardsstats.web.websocket
 
 import com.billiardsstats.read.user.User
-import com.billiardsstats.web.toJson
-import com.billiardsstats.web.websocket.protocol.out.MessageOut
-import com.billiardsstats.web.websocket.protocol.out.MessageOutType.PING
 import org.eclipse.jetty.websocket.api.Session
-import org.eclipse.jetty.websocket.api.WebSocketException
-import java.util.*
-import kotlin.concurrent.scheduleAtFixedRate
 
 /**
  * @author: Knut Esten Melandsø Nekså
@@ -29,7 +23,7 @@ object SessionManager {
 
     fun users() = sessions.values
     fun sessions() = sessions.keys
-    fun session(user: User) = sessions.entries.find{ entries -> entries.value == user }!!.key
-    fun  containsSession(session: Session) = sessions.containsKey(session)
+    fun session(user: User) = sessions.entries.find { entries -> entries.value == user }!!.key
+    fun containsSession(session: Session) = sessions.containsKey(session)
 
 }
